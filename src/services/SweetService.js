db288c208eb8c15a832a6ca5850741ae1989e57e
jaydeep <jaydeepprajapati243@gmail.com>
2025-07-16 17:05:58 +0530

    🟢 GREEN: Add validation for required fields in sweet object

--- src/services/SweetService.js
+++ src/services/SweetService.js
@@ -4,6 +4,18 @@ class SweetService {
   }
 
   addSweet(sweet) {
+    const requiredFields = ["id", "name", "category", "price", "quantity"];
+    for (let field of requiredFields) {
+      if (!sweet.hasOwnProperty(field)) {
+        throw new Error("Invalid sweet object");
+      }
+    }
+
+    const exists = this.sweets.find((s) => s.id === sweet.id);
+    if (exists) {
+      throw new Error("Sweet ID must be unique");
+    }
+
     this.sweets.push(sweet);
   }
 
